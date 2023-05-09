9a43f954fc7136111c3b842fe616970556913b1b
yum.co <101381228+yumco@users.noreply.github.com>
2023-05-09 16:12:55 +0900

    2023/05/09
    create todo list with js
    remove todo list button

--- background.js
+++ background.js
@@ -1,16 +1,16 @@
 const images = [
+    "8.jpg",
     "1.jpg",
-    "2.jpg",
+    "7.jpg",
     "3.jpg",
-    "4.jpg",
-    "5.jpeg"
+    "4.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `/daily__coding/js/img${chosenImage}`;
+bgImage.src = `js/img/${chosenImage}`;
 
 console.log(bgImage);
 
