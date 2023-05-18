c01a031b00bcb9c859c34bd816caa7a8eae644c7
yum.co <101381228+yumco@users.noreply.github.com>
2023-05-18 23:04:32 +0900

    2023/05/18
    api but error! ;(

--- weather.js
+++ weather.js
@@ -1,11 +1,14 @@
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude; 
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude; 
+    console.log("You live in", lat, lon);
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.");
 }
+
+const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+console.log(url);
 
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
